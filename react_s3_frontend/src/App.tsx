354b52428437b271bf43c4b0e2ed00517a13a5e5
Stella Tsogtjargal <stella_tsogtjargal@brown.edu>
2024-10-06 10:17:24 -0400

    feat: start pods

--- react_s3_frontend/src/App.tsx
+++ react_s3_frontend/src/App.tsx
@@ -1,8 +1,7 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './Home';
-import Pod from './pod';
-import PodID from './podId';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import Pod from "./pod";
+import PodID from "./podId";
 
 const App: React.FC = () => {
   return (
